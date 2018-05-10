f3e3a926840b79aa697aef8c5c253ab71de8090e
Guerrero Flores <r.guerrero.flores@accenture.com>
2018-05-09 21:55:54 -0500

    Adding Jenkins github plugin

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -2,6 +2,6 @@ package com;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld v2.5");
+        System.out.println("HelloWorld v2.5"
     }
 }
