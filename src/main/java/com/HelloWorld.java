02984737b6d6fe1bf13cefe529348813f44ae51b
Guerrero Flores <r.guerrero.flores@accenture.com>
2018-05-10 12:02:18 -0500

    Injecting error

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -2,6 +2,6 @@ package com;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld v3.0");
+        System.out.println("HelloWorld v3.0")
     }
 }
