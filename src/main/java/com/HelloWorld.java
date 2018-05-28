a1512e2ea970c38e7d5e1849963f91bbb816d9c8
Guerrero Flores <r.guerrero.flores@accenture.com>
2018-05-28 11:47:26 -0500

    New build

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -3,6 +3,6 @@ package com;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("HelloWorld v3.1");
+        System.out.println("HelloWorld v3.2");
     }
 }
