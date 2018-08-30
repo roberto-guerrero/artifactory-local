2462870dbcf7705118a140b946f71336258f80d9
Guerrero Flores <r.guerrero.flores@accenture.com>
2018-08-29 21:26:39 -0500

    Testing artifactory cloud

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -3,6 +3,7 @@ package com;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("HelloWorld v3.2");
+        System.out.println("Hello");
     }
+
 }
