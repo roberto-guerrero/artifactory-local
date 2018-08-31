72396829e610c4e00f88f0102e497cda2f6542f6
Guerrero Flores <r.guerrero.flores@accenture.com>
2018-08-31 09:34:24 -0500

    Updating text message

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -3,7 +3,7 @@ package com;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello Artifactory AWS!!");
+        System.out.println("Hello Artifactory AWS!!!");
     }
 
 }
